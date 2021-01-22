0fc9c548e2ec0587af11292c016a0a0ebb7532b4
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-01-22 09:11:24 +0200

    Add css to Header

--- humalasalo/Components/Header.js
+++ humalasalo/Components/Header.js
@@ -1,5 +1,7 @@
+import styles from './Header.module.css';
+
 const Header = () => (
-    <div>
+    <div className={styles.header}>
         <h1>Humalasalon pitäjä</h1>
         <h2>-Hämeen keskiaikaseura ry</h2>
     </div>
