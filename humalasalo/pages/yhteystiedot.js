babfc91eeda8b78b310436ec51867d080a92e755
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-02-11 15:57:41 +0200

    Add text to Yhteystiedot and adjust CSS of Contact

--- humalasalo/pages/yhteystiedot.js
+++ humalasalo/pages/yhteystiedot.js
@@ -10,6 +10,11 @@ const Yhteystiedot = () => (
       </Head>
       <div className={styles.main}>
         <h1>Yhteystiedot</h1>
+            <p>Vastaamme mielellämme kysymyksiin, joita teille saattaa tulla meidän harrastamisestamme. 
+              Tässä on lista meidän virkailijoistamme eli seuramme aktiiveista. 
+              Jos asiasi koskee jotain tiettyä aihealuetta, kannattaa lähestyä suoraan kyseisen aihealueen asiantuntijaa, 
+              mutta tarvittaessa ohjaamme kysymykset oikeaan suuntaan. 
+              Meidän toiminnastamme tiedotamme tarkemmin sähköpostilistalla sekä Facebook-ryhmässä.</p>
             <ContactInfo 
             title="Vouti (Puheenjohtaja)"
             name="Erhart von Mannheim (Jarno Manninen)"
