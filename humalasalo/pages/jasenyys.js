3f10b5174620a9349ee23e88a7a1378e5cb838e9
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-02-12 17:00:32 +0200

    Add text to jasenyys

--- humalasalo/pages/jasenyys.js
+++ humalasalo/pages/jasenyys.js
@@ -9,6 +9,17 @@ import styles from './MainContent.module.css';
       </Head>
       <div className={styles.main}>
         <h1>Jäsenyys</h1>
+        <p>Hämeen keskiaikaseuraan liitytään liittymällä Suomen keskiaikaseuraan. 
+          Tulostettavan jäsenlomakkeen löydät täältä. Lomakkeesta valitaan paikallisyhdistykseksi Hämeen keskiaikaseura. 
+          Keskiaikanimi ei ole pakollinen, jos sellaista ei ole ehtinyt itselleen kehittää. 
+          Jäsenmaksu sisältää Suomen keskiaikaseuran oman julkaisun Aarniwalcean sekä Drachenwaldin eli Euroopan laajuisen 
+          kuningaskunnan julkaisun Dragon’s Talen sähköisessä muodossa.
+        </p>
+        <p>Hämeen keskiaikaseura ei vaadi jäsenyyttä paikallistoiminnassa tai tapahtumissa, 
+          mutta jäsenenä saat alennusta kaikkien Suomen keskiaikaseuran alaisten keskiaikatapahtumien tapahtumamaksuista. 
+          Viikonlopputapahtumista alennus on tyypillisesti viisi euroa. Lisäksi vain maksaneet jäsenet lasketaan pitäjän varsinaisiksi asukkaiksi, 
+          jotka saavat vaikuttaa seuran sisäisiin asioihin kuten paronillisten valintaan tai asettua itse ehdolle. 
+        </p>
       </div>
     </Layout> 
 
