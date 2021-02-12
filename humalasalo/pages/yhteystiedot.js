43ce72d0c5e726c52ba3e611893c081e650cace2
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-02-12 09:30:09 +0200

    Change margins and text

--- humalasalo/pages/yhteystiedot.js
+++ humalasalo/pages/yhteystiedot.js
@@ -10,7 +10,7 @@ const Yhteystiedot = () => (
       </Head>
       <div className={styles.main}>
         <h1>Yhteystiedot</h1>
-            <p>Vastaamme mielellämme kysymyksiin, joita teille saattaa tulla meidän harrastamisestamme. 
+            <p>Vastaamme mielellämme kysymyksiin, joita teille saattaa tulla harrastuksestamme. 
               Tässä on lista meidän virkailijoistamme eli seuramme aktiiveista. 
               Jos asiasi koskee jotain tiettyä aihealuetta, kannattaa lähestyä suoraan kyseisen aihealueen asiantuntijaa, 
               mutta tarvittaessa ohjaamme kysymykset oikeaan suuntaan. 
