9f942b7e9842a93c2a5931a753680ae42973bed0
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-02-19 17:53:13 +0200

    Add vaakuna2.png and add it to FooterPic

--- humalasalo/Components/FooterPic.js
+++ humalasalo/Components/FooterPic.js
@@ -1,6 +1,13 @@
-const FooterPic = (props) => (
-    <div>
+import Image from 'next/image';
 
+const FooterPic = () => (
+    <div>
+        <Image 
+            src="/images/vaakuna2.png"
+            alt="Punainen kultakuvioinen vaakuna kilvellä, miekat ristissä takana"
+            height="100"
+            width="100"
+        />
     </div>
 )
 export default FooterPic;
