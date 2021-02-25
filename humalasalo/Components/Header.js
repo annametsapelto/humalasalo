75772b59db7d1e509f1111da059208c52af5a03c
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-02-25 09:20:20 +0200

    Work with the header and margins

--- humalasalo/Components/Header.js
+++ humalasalo/Components/Header.js
@@ -1,7 +1,14 @@
 import styles from './Header.module.css';
+import Image from 'next/image';
 
 const Header = () => (
     <div className={styles.header}>
+        <Image className={styles.picture} 
+            src="/images/vaakuna2.png"
+            alt="Punainen kultakuvioinen vaakuna kilvellä, miekat ristissä takana"
+            height="150"
+            width="120"
+        />
         <h1>Humalasalon pitäjä</h1>
         <h2>-Hämeen keskiaikaseura ry</h2>
     </div>
