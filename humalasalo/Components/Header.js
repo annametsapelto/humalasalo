973b12906b5b9c6d30b66c5d21e0c38b8a8f83b9
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-02-25 10:13:04 +0200

    Remove picture from Header

--- humalasalo/Components/Header.js
+++ humalasalo/Components/Header.js
@@ -1,14 +1,7 @@
 import styles from './Header.module.css';
-import Image from 'next/image';
 
 const Header = () => (
     <div className={styles.header}>
-        <Image className={styles.picture} 
-            src="/images/vaakuna2.png"
-            alt="Punainen kultakuvioinen vaakuna kilvellä, miekat ristissä takana"
-            height="150"
-            width="120"
-        />
         <h1>Humalasalon pitäjä</h1>
         <h2>-Hämeen keskiaikaseura ry</h2>
     </div>
