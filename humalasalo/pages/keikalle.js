001a6869f7642aa3f0fb71ca28bdfed9633cf56b
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-03-06 19:40:21 +0200

    Add text to Keikalle

--- humalasalo/pages/keikalle.js
+++ humalasalo/pages/keikalle.js
@@ -9,6 +9,16 @@ import styles from './MainContent.module.css';
       </Head>
       <div className={styles.main}>
         <h1>Keikalle</h1>
+        <p>Hämeen keskiaikaseuraa voi kysyä erilaisiin esiintymistilaisuuksiin. 
+          Olemme esiintyneet useilla eri historiallisilla markkinoilla, 
+          muissa yleisissä tilaisuuksissa sekä myös yksityistilaisuuksissa. 
+          Tyypillisimmät esitykset ovat tanssi- sekä taisteluesitykset. 
+          Meillä ei ole tarkkaa hinnoittelua esityksille, vaan hinta sovitaan 
+          tulijoiden määrän, esitysten pituuden, matkan pituuden sekä 
+          esiintymistilaisuuden luonteen mukaan. Jos olette kiinnostuneet 
+          kysymään meitä esiintymään tapahtumaanne, ottakaa hyvissä ajoin 
+          yhteyttä puheenjohtajaamme
+         sähköpostilla osoitteeseen vouti@humalasalo.net.</p>
       </div>
     </Layout>
 
