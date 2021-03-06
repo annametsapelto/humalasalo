c8ea91df757e1b50aa438a5ecd0314115893da8e
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-03-06 20:00:30 +0200

    Add text and a picture to Seuralaisille

--- humalasalo/pages/seuralaisille.js
+++ humalasalo/pages/seuralaisille.js
@@ -1,4 +1,5 @@
 import Layout from '../Components/Layout';
+import LargePicture from '../Components/LargePicture';
 import Head from 'next/head';
 import styles from './MainContent.module.css';
 
@@ -9,6 +10,14 @@ const Seuralaisille = () => (
       </Head>
       <div className={styles.main}>
         <h1>Seuralaisille</h1>
+        <p>Humalasalon väellä on käytettävissään seuran oma kirjasto, 
+          joka sijaitsee Märtan ja Lukasin residenssissä. 
+          Kirjat ovat lainattavissa kaikille Humalasalon vakiintuneille 
+          asukkaille eli maksaville jäsenille sekä pidempään mukana olleille ei-maksaneille. </p>
+        <LargePicture
+            picture = "/images/käsityöt3.jpg"
+            name = "Nyörihaarukka ja lankaa."
+        />
       </div>
     </Layout>
 
