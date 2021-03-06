23dce696c858ae4f9fb9fc3a3ddca0f5a13c8716
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-03-06 20:34:34 +0200

    Create SmallPicture and css

---
+++ humalasalo/Components/SmallPicture.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import styles from './SmallPicture.module.css';
+
+const SmallPicture = (props) => {
+    <div className={styles.spic}>
+        <Image
+            src={props.picture}
+            alt={props.name}
+            width={300}
+            height={100}
+        />
+    </div>
+
+}
+ export default SmallPicture;
