cbbc6d0defce06c9c14e16e648e19c3dcfa61ed4
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-03-07 10:32:04 +0200

    Add and adjust a small picture

--- humalasalo/Components/SmallPicture.js
+++ humalasalo/Components/SmallPicture.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from './SmallPicture.module.css';
 
 const SmallPicture = (props) => (
-    <div className={styles.spic}>
+    <div className = {styles.smallPic}>
     <Image
         src={props.picture}
         alt={props.name}
