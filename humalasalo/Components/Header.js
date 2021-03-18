d21be0efdcb8bb4895f85fa163e84dd39ad71e8b
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-03-18 15:59:35 +0200

    Add picture to Header

--- humalasalo/Components/Header.js
+++ humalasalo/Components/Header.js
@@ -1,9 +1,20 @@
 import styles from './Header.module.css';
+import Image from 'next/image';
 
 const Header = () => (
-    <div className={styles.header}>
-        <h1>Humalasalon pitäjä</h1>
-        <h2>-Hämeen keskiaikaseura ry</h2>
+    <div className = {styles.head}>
+    <div className = {styles.device}>
+        <Image
+            src="/Images/humalasalodevice.png"
+            alt="Humalasalon vaakuna"
+            width={300}
+            height={450}
+        />
+    </div>
+        <div className={styles.header}>
+            <h1>Humalasalon pitäjä</h1>
+            <h2>-Hämeen keskiaikaseura ry</h2>
+        </div>
     </div>
 )
 
