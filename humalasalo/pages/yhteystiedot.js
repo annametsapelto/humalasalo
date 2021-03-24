20f4a7b1b600fe8e535df375a6cc399a93548232
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-03-24 19:58:19 +0200

    Add all correct badges

--- humalasalo/pages/yhteystiedot.js
+++ humalasalo/pages/yhteystiedot.js
@@ -25,92 +25,92 @@ const Yhteystiedot = () => (
             />
             <ContactInfo 
                 title="Varavouti (Varapuheenjohtaja)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/humalasalodeviceplain.png"
+                picName = "Humalasalon vaakuna"
                 name="Dubhghall MacÉibhearáird (Henri Laine)"
                 email="Email: varavouti@humalasalo.net"
             />
             <ContactInfo 
                 title="Rahainvartija (Rahastonhoitaja)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/exchequer-small.png"
+                picName = "Rahainvartijan vaakuna"
                 name="Humalasalon Tuikku (Tuikku Pirhonen)"
                 email="Email: rahainvartija@humalasalo.net"
             />
             <ContactInfo 
                 title="Kastellaani (Jäsenvastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/chatelaine-small.png"
+                picName = "Kastellaanin vaakuna"
                 name="Dianora del Bianco (Anna Metsäpelto)"
                 email="Email: kastellaani@humalasalo.net"
             />
             <ContactInfo 
                 title="Jousimarski (Jousiammuntavastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/archery-small.png"
+                picName = "Jousimarskin vaakuna"
                 name="Anneke Alfintytär Lindu (Anne Syrjä)"
                 email="Email: jousimarski@humalasalo.net"
             />
             <ContactInfo 
                 title="Marski (Taisteluvastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/heavy-small.png"
+                picName = "Marskin vaakuna"
                 name="Oliver of Harjuwalcea (Olli Leimu)"
                 email="Email: marski@humalasalo.net"
             />
             <ContactInfo 
                 title="Airut (Vaakuna- ja nimivastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/herald-small.png"
+                picName = "Airueen vaakuna"
                 name="Joanne am Rein (Hanna Kivelä)"
                 email="Email: airut@humalasalo.net"
             />
             <ContactInfo 
                 title="Tiede- ja taideneuvos"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/a&s-small.png"
+                picName = "Neuvoksen vaakuna"
                 name="()"
                 email="Email: moas@humalasalo.net"
             />
             <ContactInfo 
                 title="Tervakannaksen kylänvanhin (Tampereen paikallisvastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/humalasalodeviceplain.png"
+                picName = "Humalasalon vaakuna"
                 name="Ósk Grimsdóttir (Onerva Leisti)"
                 email="Email: kv-tervakannas@humalasalo.net"
             />
             <ContactInfo 
                 title="Tammilinnan kylänvanhin (Hämeenlinnan paikallisvastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/humalasalodeviceplain.png"
+                picName = "Humalasalon vaakuna"
                 name="(Joni Karjalainen)"
                 email="Email: kv-tammilinna@humalasalo.net"
             />
             <ContactInfo 
                 title="Harjuvakean kylänvanhin (Valkeakosken paikallisvastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/humalasalodeviceplain.png"
+                picName = "Humalasalon vaakuna"
                 name="Harjuvalkean Jaakko (Jaakko Voutilainen)"
                 email="Email: kv-harjuvalkea@humalasalo.net"
             />
             <ContactInfo 
                 title="Sudenportin kylänvanhin (Lahden paikallisvastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/humalasalodeviceplain.png"
+                picName = "Humalasalon vaakuna"
                 name="()"
                 email="Email: kv-sudenportti@humalasalo.net"
             />
             <ContactInfo 
                 title="Tanssimestari"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/humalasalodeviceplain.png"
+                picName = "Humalasalon vaakuna"
                 name="Adrian Möhkö (Atro Kajaste)"
                 email="Email: tanssimestari@humalasalo.net"
             />
             <ContactInfo 
                 title="Seitinkutoja (Web-vastaava)"
-                pic = "/images/seneschal-small.png"
-                picName = "Voudin vaakuna"
+                pic = "/images/web-small.png"
+                picName = "Seitinkutojan vaakuna"
                 name="Dianora del Bianco (Anna Metsäpelto)"
                 email="Email: webvastaava@humalasalo.net"
             />
