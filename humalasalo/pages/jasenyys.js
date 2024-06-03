8cda3795fbd492f24072dc85f2d52815bf591856
Moiranen <moira@iki.fi>
2024-06-03 18:49:24 +0300

    Remove mention of newsletters
    Newsletters are no longer included in membership as they are free.

--- humalasalo/pages/jasenyys.js
+++ humalasalo/pages/jasenyys.js
@@ -19,11 +19,9 @@ import Link from 'next/link';
         />
         <p>Hämeen keskiaikaseuraan liitytään liittymällä Suomen keskiaikaseuraan. 
           Tulostettavan jäsenlomakkeen löydät <Link href="https://www.aarnimetsa.org/keskiaikaseura-yhdistyksena/lomakkeet/" target="_blank">täältä</Link>. Lomakkeesta valitaan paikallisyhdistykseksi Hämeen keskiaikaseura. 
-          Keskiaikanimi ei ole pakollinen, jos sellaista ei ole ehtinyt itselleen kehittää. 
-          Jäsenmaksu sisältää Suomen keskiaikaseuran oman julkaisun Aarniwalcean sekä Drachenwaldin eli Euroopan laajuisen 
-          kuningaskunnan julkaisun Dragon’s Talen sähköisessä muodossa.
+          Keskiaikanimi ei ole pakollinen, jos sellaista ei ole ehtinyt itselleen kehittää.
         </p>
-        <p>Hämeen keskiaikaseura ei vaadi jäsenyyttä paikallistoiminnassa tai tapahtumissa, 
+        <p>Hämeen keskiaikaseura ei edellytä jäsenyyttä paikallistoiminnassa tai tapahtumissa, 
           mutta jäsenenä saat alennusta kaikkien Suomen keskiaikaseuran alaisten keskiaikatapahtumien tapahtumamaksuista. 
           Viikonlopputapahtumista alennus on tyypillisesti viisi euroa. Lisäksi vain maksaneet jäsenet lasketaan pitäjän varsinaisiksi asukkaiksi, 
           jotka saavat vaikuttaa seuran sisäisiin asioihin kuten paronillisten valintaan tai asettua itse ehdolle. 
